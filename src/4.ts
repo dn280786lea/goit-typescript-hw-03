e309a874fffb1628cc7461e400de578f3fbeecba
dn280786lea <ekaterina.lubenets@gmail.com>
2024-01-02 15:10:39 +0100

    up
    up

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,50 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-const house = new MyHouse(key);
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(door: boolean, key: Key) {
+    this.door = door;
+    this.key = key;
+  }
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+}
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
+const house = new MyHouse(false, key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
